937230fc5b8aaad139ef2a30f61296a0951214af
Danzer-bit <56238078+Danzer-bit@users.noreply.github.com>
2025-04-03 13:52:04 +0200

    Add files via upload

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2207784.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 220851950, 220851944, 220851948, 220851939, 220851967, 220851968, 220851955, 220851970 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/220851935/1/basis.wasm.js', 'wasmUrl' : 'files/assets/220851933/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/220851940/1/basis.js', 'preload' : false},
+];
